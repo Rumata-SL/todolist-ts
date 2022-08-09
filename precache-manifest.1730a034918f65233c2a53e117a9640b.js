4e2e05fdcf240a53c67bae886bea41440268e122
Rumata-SL <kirr7619@gmail.com>
2022-08-09 23:05:40 +0500

    Updates

---
+++ precache-manifest.1730a034918f65233c2a53e117a9640b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc1697533508dbc7896f8d483341181",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "d126d6542c2bd4da5d3c",
+    "url": "/todolist-ts/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "a4ed863520a51f607abb",
+    "url": "/todolist-ts/static/js/2.30337d82.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-ts/static/js/2.30337d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d126d6542c2bd4da5d3c",
+    "url": "/todolist-ts/static/js/main.e2035eaa.chunk.js"
+  },
+  {
+    "revision": "b7c7c0b009241d8831c6",
+    "url": "/todolist-ts/static/js/runtime-main.8e0caec2.js"
+  }
+]);
